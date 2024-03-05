44dadeb26e3c48bfe9c0ce70b73c60315bff278d
M.Mohie <mohamedmohie764@gmail.com>
2024-03-05 14:57:23 +0200

    fixing Typeofy function to return actuial type

--- src/lib/typeofy.ts
+++ src/lib/typeofy.ts
@@ -1,11 +1,20 @@
 import { FieldObject } from '../types'
 
-export function Typeofy(fields: FieldObject[]): Record<string, string | number | boolean> {
-  const dynamicType: Record<string, string | number | boolean> = {}
+type ValueForType<T extends FieldObject['type']> = T extends 'text' | 'textarea'
+  ? string
+  : T extends 'number'
+    ? number
+    : T extends 'checkbox'
+      ? boolean
+      : T extends 'file'
+        ? File
+        : never
 
-  fields.forEach((field) => {
-    dynamicType[field.name] = field.value
-  })
-
-  return dynamicType
+function generateType<T extends FieldObject>(field: T): { [K in T['name']]: ValueForType<T['type']> } {
+  return {
+    [field.name]: field.value as ValueForType<T['type']>,
+  } as { [K in T['name']]: ValueForType<T['type']> }
+}
+export function Typeofy(fields: FieldObject[]) {
+  return typeof fields.map((field) => generateType(field))
 }
